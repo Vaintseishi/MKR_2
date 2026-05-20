2b8d2a6a6519e0cb7642dce074880cd1ee331ebe
Корзун Олександр <olexandr.korzun@gmail.com>
2026-05-20 18:10:42 +0300

    feat: add recipe_detail view to display recipe by id

--- recipe/views.py
+++ recipe/views.py
@@ -5,3 +5,8 @@ def main(request):
     # Фільтруємо рецепти, де рік створення дорівнює 2023
     recipes = Recipe.objects.filter(created_at__year=2023)
     return render(request, 'main.html', {'recipes': recipes})
+
+def recipe_detail(request, id):
+    # Отримуємо рецепт за id або повертаємо 404
+    recipe = get_object_or_404(Recipe, id=id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
